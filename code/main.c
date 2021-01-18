7c8a4ebffe07309941e25c776d049466e9c6175b
Persano <persano@persanol.computer>
2021-01-18 16:20:51 +0100

    ajout package de base

---
+++ code/main.c
@@ -0,0 +1,21 @@
+
+
+/**
+ * @file main.c
+ * @author Persano Luca
+ * @brief Main code
+ *
+ */
+
+ #include <stdio.h>
+int main (){
+  
+
+  printf("Automatic Courses Player\n");
+  // Appel à la fonction agenda
+
+
+
+
+
+}
